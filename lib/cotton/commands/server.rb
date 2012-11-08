03063b27bcdfb1482ea399c3ede1929906f08fe6
Sam Smoot <ssmoot@gmail.com>
2012-11-08 08:12:18 -0600

    Stubbing out cotton server command.

---
+++ lib/cotton/commands/server.rb
@@ -0,0 +1,31 @@
+class Cotton::CLI::Commands::Server < Cotton::CLI
+
+  def self.summary
+    <<-EOS.margin
+      Start a web server for your project.
+    EOS
+  end
+
+  def self.options
+    Options.new do |options|
+      options.banner = "Usage: cotton server"
+      options.separator ""
+      options.separator "Options"
+
+      options.port = 9292
+      options.on "-p", "--port [PORT_NUMBER]", "Start the server on a non-standard port (default is 9292)." do |port|
+        options.port = port
+      end
+
+      options.separator ""
+      options.separator "Summary: #{summary}"
+    end
+  end
+
+  def self.start(args)
+    options = self.options.parse!(args)
+    # doubleshot = Doubleshot::current
+
+    return 0
+  end
+end
