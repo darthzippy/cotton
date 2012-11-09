912d9a38697d375f928267643a16c15591a1f225
Sam Smoot <ssmoot@gmail.com>
2012-11-09 15:30:10 -0600

    Updated ResourceHandler.java source link to Jetty stable-8. tag #11

--- ext/java/PublicFileHandler.java
+++ ext/java/PublicFileHandler.java
@@ -4,7 +4,7 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 
 // See ResourceHandler source here:
 //
-//   http://download.eclipse.org/jetty/stable-7/xref/org/eclipse/jetty/server/handler/ResourceHandler.html
+//   http://download.eclipse.org/jetty/stable-8/xref/org/eclipse/jetty/server/handler/ResourceHandler.html
 //
 // We need to override it's behavior to skip directory listing, and not handle the request.
 // That way if the request matches an existing directory (root will always match, which is the main problem),
