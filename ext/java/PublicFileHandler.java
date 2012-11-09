a354691ad5230bdb6a1365af90e306f0064aaa61
Sam Smoot <ssmoot@gmail.com>
2012-11-09 15:28:53 -0600

    Notes on PublicFileHandler for #11.

--- ext/java/PublicFileHandler.java
+++ ext/java/PublicFileHandler.java
@@ -2,6 +2,16 @@ package org.sam.cotton;
 
 import org.eclipse.jetty.server.handler.ResourceHandler;
 
+// See ResourceHandler source here:
+//
+//   http://download.eclipse.org/jetty/stable-7/xref/org/eclipse/jetty/server/handler/ResourceHandler.html
+//
+// We need to override it's behavior to skip directory listing, and not handle the request.
+// That way if the request matches an existing directory (root will always match, which is the main problem),
+// instead of getting a 403 like now, the Handler will pass, and our HandlerCollection will invoke
+// our Dispatcher instead.
+//
+// The PublicFileHandler should be used ONLY for serving static FILES.
 public class PublicFileHandler extends ResourceHandler {
 
   public PublicFileHandler() {
